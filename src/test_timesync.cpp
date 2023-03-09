60638abc26a5516e9199b94169e1f02da735cba7
chengguizi <cgz_05@sina.com>
2023-03-09 20:53:11 +0800

    fix ms us unit

--- src/test_timesync.cpp
+++ src/test_timesync.cpp
@@ -91,13 +91,13 @@ int main(int argc, char** argv)
 
     telemetry.subscribe_scaled_imu(
         [system] (Telemetry::Imu imu_data) {
-            uint64_t timestamp_us =  imu_data.timestamp_us - system->get_timesync_offset_ns() / 1e6;
+            uint64_t timestamp_us =  imu_data.timestamp_us - system->get_timesync_offset_ns() / 1e3;
             spdlog::info("imu scaled raw_ts = {} ms,  host_ts = {} ms", imu_data.timestamp_us / 1e3, timestamp_us / 1e3);
     });
 
     telemetry.subscribe_imu(
         [system] (Telemetry::Imu imu_data) {
-            uint64_t timestamp_us =  imu_data.timestamp_us - system->get_timesync_offset_ns() / 1e6;
+            uint64_t timestamp_us =  imu_data.timestamp_us - system->get_timesync_offset_ns() / 1e3;
             spdlog::info("imu highres raw_ts = {} ms,  host_ts = {} ms", imu_data.timestamp_us / 1e3, timestamp_us / 1e3);
         }
     );
